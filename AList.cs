36cdef75215ae92aac088d63003c5690b1e5655d
Ammar Gomaa <ammar.gomaa@cali-ts.com>
2024-09-18 08:02:27 +0300

    implement AList class

---
+++ AList.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOwnList
+{
+    public class AList<T>
+    {
+        T[] arr;
+        int currentIndex;
+
+        public AList()
+        {
+            arr = new T[10];
+            currentIndex = -1;
+        }
+
+        // Add new element to the list
+        public void AddElement(T element)
+        {
+            if (++currentIndex < arr.Length - 1)
+            {
+                //++currentIndex;
+                arr[currentIndex] = element;
+            }
+            else
+            {
+                Extended();//extend the array when reach the max size
+            }
+        }
+        //return the elemet on some index
+        public T GetElementAt(int index)
+        {
+            if (index > -1 && index <= currentIndex)
+            {
+                return arr[index];
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+        //remove the elemet on some index
+        public void RemoveElementAt(int index) 
+        {
+            if(index > -1 && index <= currentIndex)
+            {
+                Shift(index);//shiftting the values 
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+        }
+        public int Size()
+        {
+            return currentIndex + 1;
+        }
+
+        private void Shift(int index)
+        {
+            for (int i = index; i < currentIndex; i++) 
+            {
+                arr[i] = arr[i+1];
+            }
+            currentIndex--;
+        }
+
+        private void Extended()
+        {
+            T[] newArr= new T[currentIndex + 20];
+            
+            for(int i=0;i<=currentIndex;i++)
+            {
+                newArr[i] = arr[i];
+            }
+            arr = newArr;
+        }
+
+        public void print()
+        {
+            foreach(T i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,29 @@ namespace MyOwnList
     {
         static void Main(string[] args)
         {
+            AList<int> list= new AList<int>();
+            list.AddElement(2);
+            list.AddElement(3);
+            list.AddElement(4);
+            list.AddElement(5);
+            list.AddElement(6);
+            Console.WriteLine("The size is " + list.Size());
+            list.AddElement(2);
+            list.AddElement(3);
+            list.AddElement(4);
+            list.AddElement(5);
+            list.AddElement(6);
+            // list.RemoveElementAt(0);
+            Console.WriteLine("The size is " + list.Size());
+            list.AddElement(4);
+            list.AddElement(5);
+            list.AddElement(6);
+            Console.WriteLine("The size is " + list.Size());
+
+            //Console.WriteLine("the element"+ list.GetElementAt(0)); 
+            Console.ReadKey();
+
+
         }
     }
 }
